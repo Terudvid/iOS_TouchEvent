33354ebd8930b92249ebbd3f3ead0cf9a9e54da4
Teru <teru.sms01@gmail.com>
2015-11-09 22:47:52 +0900

    Initial Commit

---
+++ DragTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DragTutorial
+//
+//  Created by 輝幸友金 on 2015/11/09.
+//  Copyright © 2015年 wimmity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
